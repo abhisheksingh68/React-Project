e531b1435a5dad244ba546d83cd9c44786ce3a83
abhishek singh <abhi212068@gmail.com>
2024-03-13 00:38:00 +0530

    react projects

---
+++ React-Portfolio/src/components/Contact/Contact.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+import styles from "./Contact.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container} >
+      <div className={styles.text}>
+        <h2>Contact</h2>
+        <p>Feel free to reach out!</p>
+      </div>
+      <ul className={styles.links}>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/emailIcon.png")} alt="Email icon" />
+          <a href="mailto:abhi212068@email.com">abhi212068@email.com</a>
+        </li>
+        <li className={styles.link}>
+          <img
+            src={getImageUrl("contact/linkedinIcon.png")}
+            alt="LinkedIn icon"
+          />
+          <a href="https://www.linkedin.com/in/abhisheksingh68/">linkedin.com/abhishek-kumar-singh</a>
+        </li>
+        <li className={styles.link}>
+          <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
+          <a href="https://www.github.com/abhisheksingh68">github.com/abhisheksingh68</a>
+        </li>
+      </ul>
+    </footer>
+  );
+};
